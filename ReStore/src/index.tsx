5ce7fb26263ff31a809772a78ae0fe8a41ae7b4f
AKOZACHENKO <wilddangerousbee@gmail.com>
2022-04-17 14:29:06 +0300

    delete not used imports

--- ReStore/src/index.tsx
+++ ReStore/src/index.tsx
@@ -2,13 +2,11 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/app/app";
 import ErrorBoundary from "./components/error-boundry/error-boundry";
-import Spinner from './components/spinner/spinner'
-import { BrowserRouter, Route, Router, Routes } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 
 import { Provider } from "react-redux";
 
 import store from "./store";
-import PageNotFound from "./pages/page-not-found";
 
 ReactDOM.render(
   <Provider store={store}>
