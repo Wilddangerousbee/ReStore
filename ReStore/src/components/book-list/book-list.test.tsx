d359dac1a1c6f1d2e6b17a9fdf955ea941f1286d
AKOZACHENKO <wilddangerousbee@gmail.com>
2022-04-18 05:00:58 +0300

    add provaider in book list

--- ReStore/src/components/book-list/book-list.test.tsx
+++ ReStore/src/components/book-list/book-list.test.tsx
@@ -2,20 +2,32 @@ import { render, screen } from "@testing-library/react";
 import { Book } from "../../types/types";
 import BookList from "./book-list";
 
+import store from "../../store";
+
 import '@testing-library/jest-dom/extend-expect';
+import { Provider } from "react-redux";
+import { BooksAction } from "../../actions";
 
 describe("test book list", () => {
     test('one element in array', () => { 
-        const books = [{
-            title: "Hideo",
-            author: "Kodzima"
-        }, {
-            title: "Kujo",
-            author: "Jotore"
-        }]
+
+        const action : BooksAction = {
+            type: "BOOKS_LOADED",
+            payload: [{
+                title: "Hideo",
+                author: "Kodzima"
+            }, {
+                title: "Kujo",
+                author: "Jotore"
+            }]
+        }
+
+        store.dispatch(action);
         
         render(
-            <BookList books={books}/>
+            <Provider store={store}>
+                <BookList />
+            </Provider>
         )
 
         expect(screen.queryByText("Hideo")).toBeInTheDocument();
--- ReStore/src/components/book-list/book-list.tsx
+++ ReStore/src/components/book-list/book-list.tsx
@@ -1,5 +1,7 @@
 import React, { FC, Fragment } from "react";
 
+import { connect } from "react-redux";
+
 import { Book } from "../../types/types";
 import BookListItem from "../book-list-item/book-list-item";
 interface BookListProps {
@@ -14,4 +16,8 @@ const BookList : FC<BookListProps> = ({books}) => {
     )
 }
 
-export default BookList;
+const mapStateToProps = (books : BookListProps) => {
+    return books
+}
+
+export default connect(mapStateToProps)(BookList);
