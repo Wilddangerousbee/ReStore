e77b449b665c1f45cb6193f4b2b1ba066fdf9e79
AKOZACHENKO <wilddangerousbee@gmail.com>
2022-04-22 22:38:47 +0300

    add styles

--- ReStore/src/components/pages/index.ts
+++ ReStore/src/components/pages/index.ts
@@ -1,5 +1,5 @@
-import CardPage from "./cart-page";
-import HomePage from "./home-page";
+import CardPage from "./cart-page/cart-page";
+import HomePage from "./home-page/home-page";
 import PageNotFound from "./page-not-found";
 
 export { CardPage, HomePage, PageNotFound }
--- ReStore/src/service/bookstore-service-context.tsx
+++ ReStore/src/service/bookstore-service-context.tsx
@@ -1,8 +1,6 @@
 import axios from "axios";
 
 export default class ServiceApi {
-    
-
     readonly _usrlSwapi : string = "https://www.googleapis.com/books/v1/volumes?q=computer&key=AIzaSyCexOQqGdPPQm8ZKvJ_YfDQ0mFktAbIyjg&maxResults=40";
 
     getDate = async (url = this._usrlSwapi) => {
