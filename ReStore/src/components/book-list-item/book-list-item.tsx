5ce6402174f4bb10328cde4a86f8067d03ad2a8b
AKOZACHENKO <wilddangerousbee@gmail.com>
2022-04-23 20:39:41 +0300

    change css book list

--- ReStore/src/components/book-list-item/book-list-item.tsx
+++ ReStore/src/components/book-list-item/book-list-item.tsx
@@ -20,18 +20,21 @@ const BookListItem : FC<IBookListItem> = ({id, title, author, imageLink}) => {
     }
 
     return (
-        <Link className="link-book" to={`book/${id}`}>
-            <div 
+        <div 
                 onMouseOver={addShadow} 
                 onMouseOut = {returnUsualClass}
                 className={className}
             >
+        <Link className="link-book" to={`book/${id}`}>
+            
+
+            
                     <img className="image" src={imageLink}/>
                     <p className="title">{title}</p>
                     {<p className="author">{author || "Без автора"}</p>}
-                    <button className="buy btn btn-primary btn-md">Купить</button>
-            </div>
         </Link>
+                    <button className="buy btn btn-primary btn-md">Купить</button>
+        </div>
     );
 }
 
